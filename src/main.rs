2847cca124fbb9fa492a378207cf9787677587c8
furbler <63544661+furbler@users.noreply.github.com>
2022-11-28 21:51:48 +0900

    プレイヤーが左右に動く

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::cell::RefCell;
 use std::collections::HashMap;
+use std::rc::Rc;
 use wasm_bindgen::Clamped;
 use wasm_bindgen::{prelude::Closure, JsCast, JsValue};
 use web_sys::{window, ImageBitmap, ImageData};
@@ -17,6 +19,14 @@ impl Vec2 {
         Self { x, y }
     }
 }
+
+// 対応するキーが押されている時真
+#[derive(Debug)]
+struct KeyDown {
+    left: bool,  // プレイヤーを左へ移動させる
+    right: bool, // プレイヤーを右へ移動させる
+    shot: bool,  // プレイヤーが弾を撃つ
+}
 pub enum Msg {
     // ビットマップ画像を取得
     RetBitmapImage(Vec<ImageType>, HashMap<ImageType, ImageData>, Vec<Vec<u8>>),
@@ -30,6 +40,7 @@ pub enum Msg {
     Initialize,
     MainLoop,
 }
+
 #[derive(Eq, Hash, PartialEq, Clone)]
 pub enum ImageType {
     Player,
@@ -63,7 +74,14 @@ struct Player {
 }
 
 impl Player {
-    fn update(&self) {}
+    fn update(&mut self, input_key: &KeyDown) {
+        if input_key.left {
+            self.pos.x -= 5.;
+        }
+        if input_key.right {
+            self.pos.x += 5.;
+        }
+    }
     fn render(&self, ctx: &CanvasRenderingContext2d) {
         ctx.draw_image_with_image_bitmap_and_dw_and_dh(
             &self.image.as_ref().unwrap(),
@@ -92,6 +110,7 @@ impl Enemy {
             // 動く時以外は何もしない
             return;
         }
+
         // 方向を考慮して動く
         self.pos.x += 20. * move_dir as f64;
         // 表示する画像を切り替える
@@ -120,7 +139,7 @@ struct EnemyManage {
     // 敵の左方向、右方向の移動範囲限界のx座標
     left_border: f64,
     right_border: f64,
-    // 移動方向(正の値は右、負の値は左に向かう)
+    // 移動方向(右は1、左は-1)
     move_dir: i32,
     // 次フレームで移動方向を反転すべきか否か
     move_dir_invert: bool,
@@ -242,6 +261,7 @@ struct AnimationCanvas {
     player: Player,
     enemy_manage: EnemyManage,
     callback: Closure<dyn FnMut()>,
+    input_key_down: Rc<RefCell<KeyDown>>,
 }
 
 impl Component for AnimationCanvas {
@@ -354,6 +374,11 @@ impl Component for AnimationCanvas {
                 enemys_list: Vec::new(),
             },
             callback,
+            input_key_down: Rc::new(RefCell::new(KeyDown {
+                left: false,
+                right: false,
+                shot: false,
+            })),
         }
     }
 
@@ -417,10 +442,43 @@ impl Component for AnimationCanvas {
                     image: Some(image_player),
                 };
 
+                // キー入力処理
+                let key_down = Rc::clone(&self.input_key_down);
+                let document = web_sys::window().unwrap().document().unwrap();
+                let body = document.body().unwrap();
+                let canvas = document
+                    .create_element("canvas")
+                    .unwrap()
+                    .dyn_into::<HtmlCanvasElement>()
+                    .unwrap();
+                body.append_child(&canvas).unwrap();
+                // キー押し下げ
+                let closure_key_down = Closure::wrap(Box::new(move |e: web_sys::KeyboardEvent| {
+                    input_key_down(e, &mut key_down.borrow_mut());
+                }) as Box<dyn FnMut(_)>);
+                body.add_event_listener_with_callback(
+                    "keydown",
+                    closure_key_down.as_ref().unchecked_ref(),
+                )
+                .unwrap();
+                closure_key_down.forget();
+
+                // キー押し上げ
+                let key_up = Rc::clone(&self.input_key_down);
+                let closure_key_up = Closure::wrap(Box::new(move |e: web_sys::KeyboardEvent| {
+                    input_key_up(e, &mut key_up.borrow_mut());
+                }) as Box<dyn FnMut(_)>);
+                body.add_event_listener_with_callback(
+                    "keyup",
+                    closure_key_up.as_ref().unchecked_ref(),
+                )
+                .unwrap();
+                closure_key_up.forget();
+
                 ctx.link().send_message(Msg::MainLoop);
                 true
             }
-            // 描画処理
+            // ループ
             Msg::MainLoop => {
                 self.main_loop();
                 false
@@ -455,18 +513,13 @@ impl AnimationCanvas {
         // 画像のぼやけを防ぐ
         ctx.set_image_smoothing_enabled(false);
 
-        self.player.update();
+        log::info!("key down state = {:?}", self.input_key_down);
+        // ここで実行時エラーが起きないか不安
+        self.player.update(&self.input_key_down.borrow());
         self.player.render(&ctx);
 
         self.enemy_manage.update();
         self.enemy_manage.render(&ctx);
-
-        log::info!(
-            "canvas width() = {}, canvas height() = {}",
-            canvas.width(),
-            canvas.height()
-        );
-
         window()
             .unwrap()
             .request_animation_frame(self.callback.as_ref().unchecked_ref())
@@ -492,11 +545,42 @@ async fn imagedata2bitmap(image_data: ImageData) -> Result<ImageBitmap, JsValue>
         .await
         .unwrap();
 
-    Ok(result.dyn_into::<web_sys::ImageBitmap>()?)
+    Ok(result.dyn_into::<ImageBitmap>()?)
 }
 
 fn main() {
     // デバッグ出力用
     wasm_logger::init(wasm_logger::Config::default());
     yew::start_app::<App>();
+}
+// キー押し下げ時に呼び出し
+fn input_key_down(event: web_sys::KeyboardEvent, input_data: &mut KeyDown) {
+    match &*event.key() {
+        "ArrowLeft" | "a" => {
+            input_data.left = true;
+        }
+        "ArrowRight" | "d" => {
+            input_data.right = true;
+        }
+        "Space" | "Enter" => {
+            input_data.shot = true;
+        }
+        _ => (),
+    };
+}
+
+// キー押し上げ時に呼び出し
+fn input_key_up(event: web_sys::KeyboardEvent, input_data: &mut KeyDown) {
+    match &*event.key() {
+        "ArrowLeft" | "a" => {
+            input_data.left = false;
+        }
+        "ArrowRight" | "d" => {
+            input_data.right = false;
+        }
+        "Space" | "Enter" => {
+            input_data.shot = false;
+        }
+        _ => (),
+    };
 }
