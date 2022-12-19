ae88af277b8617aaeb3b2b6298533f1efe6407cb
furbler <63544661+furbler@users.noreply.github.com>
2022-12-19 20:37:13 +0900

    ポーズ機能の不具合を修正

--- src/pause.rs
+++ src/pause.rs
@@ -1,26 +1,28 @@
 pub struct Pause {
-    pause: bool,
+    // pause: bool,
     pre_pause_button: bool,
 }
 
 impl Pause {
     pub fn new() -> Self {
         Pause {
-            pause: false,
+            // pause: false,
             pre_pause_button: false,
         }
     }
+    // ポーズ停止または解除する瞬間のみ真を返す
     pub fn toggle_pause(&mut self, pause_button: bool) -> bool {
         // ポーズボタンが押された
         if !self.pre_pause_button && pause_button {
             // ポーズ開始/解除
-            self.pause = !self.pause;
+            // self.pause = !self.pause;
             self.pre_pause_button = true;
+            return true;
         }
-        // ポーズボタンが押されなくなった
         if self.pre_pause_button && !pause_button {
+            // ポーズボタンが押されなくなった
             self.pre_pause_button = false;
         }
-        self.pause
+        false
     }
 }
